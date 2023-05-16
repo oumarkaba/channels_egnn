a94e040ae79da4c19d4c3311b924d3dda12ff894
danielTLevy <danieltlevy@gmail.com>
2023-05-16 10:58:26 -0400

    Add solar dataset

---
+++ main_solar.py
@@ -0,0 +1,207 @@
+import argparse
+import torch
+import os
+from torch import nn, optim
+import json
+import time
+import wandb
+import numpy as np
+import sys
+from solar.simulator import base_classes
+
+from solar.dataset import SolarSystemDataset
+from solar.model import EGNN_vel
+
+sys.modules['base_classes'] = base_classes
+#from tqdm import tqdm
+from tqdm import  tqdm
+
+parser = argparse.ArgumentParser(description='VAE MNIST Example')
+parser.add_argument('--exp_name', type=str, default='exp_1', metavar='N', help='experiment_name')
+parser.add_argument('--batch_size', type=int, default=100, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=1000, metavar='N',
+                    help='number of epochs to train (default: 10)')
+
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log_interval', type=int, default=1, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--test_interval', type=int, default=5, metavar='N',
+                    help='how many epochs to wait before logging test')
+parser.add_argument('--lr', type=float, default=1e-6, metavar='N',
+                    help='learning rate')
+parser.add_argument('--nf_edge', type=int, default=64, metavar='N',
+                    help='hidden features for edge mlp and messages')
+parser.add_argument('--nf_node', type=int, default=64, metavar='N',
+                    help='hidden features for node mlp')
+parser.add_argument('--nf_coord', type=int, default=64, metavar='N',
+                    help='hidden features for coordinate mlp')
+parser.add_argument('--nf', type=int, default=64, metavar='N',
+                    help='number of hidden features (not used for EGNN')
+parser.add_argument('--num_vectors', type=int, default=1, metavar='N',
+                    help='number of vector channels')
+parser.add_argument('--model', type=str, default='egnn_vel', metavar='N',
+                    help='available models: gnn, baseline, linear, linear_vel, se3_transformer, egnn_vel, rf_vel, tfn')
+parser.add_argument('--attention', type=int, default=0, metavar='N',
+                    help='attention in the ae model')
+parser.add_argument('--n_layers', type=int, default=4, metavar='N',
+                    help='number of layers for the autoencoder')
+parser.add_argument('--degree', type=int, default=2, metavar='N',
+                    help='degree of the TFN and SE3')
+parser.add_argument('--weight_decay', type=float, default=1e-7, metavar='N',
+                    help='weight decay')
+parser.add_argument('--norm_diff', type=eval, default=False, metavar='N',
+                    help='normalize_diff')
+parser.add_argument('--tanh', type=eval, default=False, metavar='N',
+                    help='use tanh')
+
+time_exp_dic = {'time': 0, 'counter': 0}
+
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+
+time_exp_dic = {'time': 0, 'counter': 0}
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def weighted_mse_loss(pred, true, original):
+    mse = torch.sum((pred - true)**2, 1)
+    weight = 1/torch.sum((original - true)**2, 1)
+    return torch.mean(mse*weight)
+    
+
+def process_data(data, edges):
+        data = [d.view(-1, d.size(2)) for d in data]
+        data = [x.type(torch.FloatTensor) for x in data]
+        data = [d.to(device) for d in data]
+        loc, vel, mass, loc_end = data
+        edges = [edges[0].to(device), edges[1].to(device)]
+
+        vel_feat = torch.sqrt(torch.sum(vel ** 2, dim=1)).unsqueeze(1).detach()
+        mass_feat = torch.log(mass).detach()
+        nodes = torch.cat([vel_feat, mass_feat], dim=1).to(device)
+        rows, cols = edges
+        edge_attr = torch.sum((loc[rows] - loc[cols])**2, 1).unsqueeze(1).to(device)
+        return nodes, loc.detach(), edges, vel, edge_attr, loc_end
+
+class BaselineModel(nn.Module):
+    def __init__(self, vel_mult=10):
+        super(BaselineModel, self).__init__()
+        self.vel_mult = vel_mult
+
+    def forward(self, nodes, loc, edges, vel, edge_attr):
+        return loc + self.vel_mult*vel
+
+def main():
+    wandb.init(config=args,
+        project="channels_egnn_solar")
+
+
+    dataset_train = SolarSystemDataset(partition='train', timestep_pred=1000)
+    dataset_val = SolarSystemDataset(partition='val', timestep_pred=1000)
+    loader_train = torch.utils.data.DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, drop_last=False)
+    loader_val = torch.utils.data.DataLoader(dataset_val, batch_size=args.batch_size, shuffle=False, drop_last=True)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #model = EGNN_vel(in_node_nf=2, in_edge_nf=1, hidden_edge_nf=64,
+    #                hidden_node_nf=64,   hidden_coord_nf=64, 
+    #                device=device, act_fn=nn.SiLU(),
+    #                n_layers=4, coords_weight=1.0,
+    #                recurrent=False, norm_diff=False,
+    #                tanh=False, num_vectors=1)
+
+    model = EGNN_vel(in_node_nf=2, in_edge_nf=1, hidden_edge_nf=args.nf_edge, 
+                        hidden_node_nf=args.nf_node, hidden_coord_nf=args.nf_coord, device=device, n_layers=args.n_layers,
+                        recurrent=True, norm_diff=args.norm_diff, tanh=args.tanh, num_vectors=args.num_vectors)
+    model = model.to(device)
+    print(model)
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    num_params = sum([np.prod(p.size()) for p in model_parameters])
+    wandb.run.summary['model_params'] = num_params
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+
+    results = {'epochs': [], 'losses': []}
+    best_val_loss = 1e8
+    best_test_loss = 1e8
+    best_epoch = 0
+
+
+
+
+    losses = []
+    batch_losses = []
+    baseline_losses = []
+    batch_baseline_losses = []
+
+    baseline_model = BaselineModel(vel_mult=10).to(device)
+    baseline_train_loss = train(baseline_model, optimizer, 0, loader_train, backprop=False)
+    baseline_val_loss = train(baseline_model, optimizer, 0, loader_val, backprop=False)
+    wandb.run.summary['train/baseline_loss'] = baseline_train_loss
+    wandb.run.summary['train/baseline_loss'] = baseline_val_loss
+
+    for epoch in range(0, args.epochs):
+        train_loss = train(model, optimizer, epoch, loader_train)
+        wandb.log({'epoch': epoch, 'train/loss': train_loss})
+        if epoch % args.test_interval == 0:
+            #train(epoch, loader_train, backprop=False)
+            val_loss = train(model, optimizer, epoch, loader_val, backprop=False)
+            #test_loss = train(model, optimizer, epoch, loader_test, backprop=False)
+            results['epochs'].append(epoch)
+            #results['losses'].append(test_loss)
+
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                #best_test_loss = test_loss
+                best_epoch = epoch
+            wandb.log({'epoch': epoch, 'val/loss': val_loss, 'val/best_loss':best_val_loss})#, 'test/best_loss':best_test_loss})
+            wandb.run.summary['best_val_loss'] = best_val_loss
+            #wandb.run.summary['best_test_loss'] = best_test_loss
+            wandb.run.summary['best_epoch'] = best_epoch
+            print("*** Best Val Loss: %.5f \t Best Test Loss: %.5f \t Best epoch %d" % (best_val_loss, best_test_loss, best_epoch))
+
+
+
+def train(model, optimizer, epoch, loader, backprop=True):
+    if backprop:
+        model.train()
+    else:
+        model.eval()
+
+    res = {'epoch': epoch, 'loss': 0, 'coord_reg': 0, 'counter': 0}
+
+    for batch_idx, data in enumerate(loader):
+        batch_size, n_nodes, _ = data[0].size()
+        edges = loader.dataset.get_edges(batch_size, n_nodes)
+        edges = loader.dataset.get_edges(batch_size, n_nodes)
+        nodes, loc, edges, vel, edge_attr, loc_end = process_data(data, edges)
+
+        optimizer.zero_grad()
+        loc_pred = model(nodes, loc, edges, vel, edge_attr)
+        
+        loss = weighted_mse_loss(loc_pred, loc_end, loc)
+        if backprop:
+            loss.backward()
+            optimizer.step()
+        res['loss'] += loss.item()*batch_size
+        res['counter'] += batch_size
+        if batch_idx % args.log_interval == 0 and (args.model == "se3_transformer" or args.model == "tfn"):
+            print('===> {} Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(loader.dataset.partition,
+                epoch, batch_idx * batch_size, len(loader.dataset),
+                100. * batch_idx / len(loader),
+                loss.item()))
+
+    if not backprop:
+        prefix = "==> "
+    else:
+        prefix = ""
+    print('%s epoch %d avg loss: %.5f' % (prefix+loader.dataset.partition, epoch, res['loss'] / res['counter']))
+
+    return res['loss'] / res['counter']
+
+
+if __name__ == "__main__":
+     main()
---
+++ solar/dataset.py
@@ -0,0 +1,61 @@
+import torch
+import numpy as np
+import pickle
+
+class SolarSystemDataset(torch.utils.data.Dataset):
+    def __init__(self, partition='train', timestep_pred=1000):
+        self.partition = partition
+        solar_system = pickle.load(open('solar/data/solar_system_data.pkl', 'rb'))
+        tot_timesteps = solar_system.get_positions().shape[0]
+        self.train_timesteps = 14016 # 0.8 year of data
+        self.val_timesteps = 1752 # 0.1 year of data
+        self.test_timesteps = 1752 # 0.1 year of data
+        self.timestep_pred = 1000 # Predict 1000 timesteps into the future
+        all_positions = solar_system.get_positions()
+        all_velocities = solar_system.get_velocities()
+        if self.partition == 'train':
+            start_t = 0
+            end_t = self.train_timesteps
+        elif self.partition == 'val':
+            start_t = self.train_timesteps
+            end_t = self.train_timesteps + self.val_timesteps
+        elif self.parition == 'test':
+            start_t = self.train_timesteps + self.val_timesteps
+            end_t = self.train_timesteps + self.val_timesteps + self.test_timesteps
+        else:
+            raise Exception("Wrong partition name %s" % self.partition)
+        self.pos = all_positions[start_t:end_t]
+        self.vel = all_velocities[start_t:end_t]
+        self.n_bodies = self.pos.shape[1]
+        self.masses = solar_system.get_masses()
+        self.edges = self.get_complete_edges(self.n_bodies)
+        self.init_pos = self.pos[:-timestep_pred]
+        self.final_pos = self.pos[timestep_pred:]
+        self.init_vel = self.vel[:-timestep_pred]
+
+    def __getitem__(self, i):
+        return self.init_pos[i], self.init_vel[i], self.masses[..., np.newaxis], self.final_pos[i]
+
+    def __len__(self):
+        return self.init_pos.shape[0]
+
+    def get_complete_edges(self, n_bodies):
+        edges = []
+        for i in range(n_bodies):
+            for j in range(n_bodies):
+                if i != j:
+                    edges.append([i, j])
+        edges_array = np.array(edges).T
+        return edges_array[0], edges_array[1]
+
+    def get_edges(self, batch_size, n_nodes):
+        edges = [torch.LongTensor(self.edges[0]), torch.LongTensor(self.edges[1])]
+        if batch_size == 1:
+            return edges
+        elif batch_size > 1:
+            rows, cols = [], []
+            for i in range(batch_size):
+                rows.append(edges[0] + n_nodes * i)
+                cols.append(edges[1] + n_nodes * i)
+            edges = [torch.cat(rows), torch.cat(cols)]
+        return edges
